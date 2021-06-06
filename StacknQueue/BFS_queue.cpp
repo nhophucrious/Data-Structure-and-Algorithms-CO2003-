19bc40dea93574093ff03c99ee77bb6cf9aa0844
Minh Dang Le <58287394+minhdang2803@users.noreply.github.com>
2021-06-07 01:46:28 +0700

    Update BFS_queue.cpp
    Making some comment

--- StacknQueue/BFS_queue.cpp
+++ StacknQueue/BFS_queue.cpp
@@ -4,15 +4,22 @@
 using namespace std;
 void bfs(vector<vector<int>> graph, int start)
 {
+    // make an array to check which vertex is visited (hash)
     vector<bool> visited(graph.size(), false);
+    // make a queue
     queue<int> q;
+    // mark first vertex visited
     visited[start] = true;
+    // push first vertex to a queue
     q.push(start);
     while (q.empty() == false)
     {
+        // print the vertext in the queue (which we already visited) and pop it
         int value = q.front();
         cout << value << " ";
         q.pop();
+        // traverse all the adjacency vertices of start vertext, if it already visited, continue to next adjacency
+        //else mark it visited and push it to a queue
         for (int i = 0; i < graph[value].size(); i++)
         {
             if (visited[graph[value][i]] == false)
