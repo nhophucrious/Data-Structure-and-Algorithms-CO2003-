a79083b7a914c90f73ef32938d89cf5ae7ec0f59
Minh Dang Le <58287394+minhdang2803@users.noreply.github.com>
2021-06-29 15:35:39 +0700

    Update isBipartite_DFS.cpp

--- StacknQueue/isBipartite_DFS.cpp
+++ StacknQueue/isBipartite_DFS.cpp
@@ -1,3 +1,34 @@
+Skip to content
+Search or jump to…
+
+Pull requests
+Issues
+Marketplace
+Explore
+ 
+@minhdang2803 
+minhdang2803
+/
+Data-Structure-and-Algorithms-CO2003-
+1
+00
+Code
+Issues
+Pull requests
+Actions
+Projects
+Wiki
+Security
+Insights
+Settings
+Data-Structure-and-Algorithms-CO2003-/StacknQueue/isBipartite_DFS.cpp
+@minhdang2803
+minhdang2803 DSA 203 isBipartite using DFS
+Latest commit 3e40a91 12 days ago
+ History
+ 1 contributor
+55 lines (51 sloc)  1.22 KB
+  
 #include<iostream>
 #include<queue>
 #include<stack>
@@ -30,21 +61,26 @@ using namesapce std;
 
 int main()
 {
- int G[6][6] = { {0, 1, 0, 0, 0, 1},
-                {1, 0, 1, 0, 0, 0},
-                {0, 1, 0, 1, 0, 0},
-                {0, 0, 1, 0, 1, 0},
-                {0, 0, 0, 1, 0, 1},
-                {1, 0, 0, 0, 1, 0} };
-int n = 6;
-
-vector<vector<int>> graph(n, vector<int>());
-	for (int i = 0; i < n; ++i) {
-		for (int j = 0; j < n; ++j) {
-			if (G[i][j]) graph[i].push_back(j);
-		}
-	}
-
-isBipartite(graph) ? cout << "Yes" : cout << "No";
+  bool dfs(int i,vector<vector<int>>& graph,vector<int> &color,int c)
+    {
+        if(color[i]!=0)return color[i]==c;
+        color[i]=c;
+        for(auto x:graph[i])
+        {
+            if(!dfs(x,graph,color,-c))
+                return false;
+        }
+        return true;
+    }
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n=graph.size();
+        vector<int> color(n,0);
+        for(int i=0;i<graph.size();i++)
+        {
+            if(color[i]==0 && !dfs(i,graph,color,1))
+                return false;
+        }
+        return true;
+    }
   return 0;
 }
